7305cd96eaf6b29c8ec2f8fb0a8048f29f6d2e94
zeldabedabomb <88691158+zeldabedabomb@users.noreply.github.com>
2022-02-02 15:16:37 -0500

    Did some work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Item from './Components/MyItem';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Item name="James"/>
         <p>
           Testing testing testing
         </p>
---
+++ src/Components/MyItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Item extends React.Component{
+
+    clickMe(){
+        console.log("I was clicked by: ", this.props.name)
+    }
+
+    render() {
+        return(
+            <h1 onClick={() => this.clickMe()}>Hello {this.props.name}!!</h1>
+        )
+    }
+}
+
+
+export default Item;
